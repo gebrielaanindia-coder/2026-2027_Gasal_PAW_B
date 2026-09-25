2450ba5e8b54ec68ab3ad3ef59c254ed71ba0752
Rizki Pratama Sunarko <gebrielaanindia@gmail.com>
2026-09-25 15:08:53 +0700

    Tugas Modul 1

---
+++ Modul-1/25-093_Gebriela Anindia Chantika/soal17.php
@@ -0,0 +1,10 @@
+<?php
+function sum($x, $y) {
+    $z = $x + $y;
+    echo "$x + $y = $z<br>";
+}
+
+echo sum(10, 5);
+echo sum(7, 13);
+echo sum(2, 4);
+?>
---
+++ Modul-1/25-093_Gebriela Anindia Chantika/soal3.php
@@ -0,0 +1,18 @@
+<!-- soal 1 -->
+<?php
+echo "Hello World";// ini non embedded script
+?>
+<br>
+<!-- soal 2 -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+        <?php
+        echo "Hello World"; // ini embedded script
+        ?> 
+
+</body>
+</html>
